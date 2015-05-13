11ac5b1722376a4f927b4a838a6028be1229a537
KotaroSawasato <mlo123349@gmail.com>
2015-05-13 21:09:58 +0900

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+	<title>ログアウト</title>
+</head>
+<body>
+	<?php
+	if($_POST["flag"]=="logout"){
+		$SESSION[]=array();
+		session_destroy();
+		echo 'ログアウト中です。５秒後にトップページへ移動します'
+		echo '<meta http-equiv="refresh" content="5;URL=../php_homework03/top.php"/>';
+	}
+
+	?>
+	
+	
+	
+
+	
+</body>
+</html>
