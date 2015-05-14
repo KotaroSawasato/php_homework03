df3ac6c7de8206f0e5e5cc27c7cf4b1ecf942c66
KotaroSawasato <mlo123349@gmail.com>
2015-05-14 17:18:09 +0900

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -2,20 +2,19 @@
 session_start();
 require_once('db_access.php');
 $dbh = accessDB();
-//ログインフォームの値がデータベースの内容と一致するか調べる
-$login_data[0]=$_POST["login_address"];
-$login_data[1]=$_POST["login_pass"];
 
-//フォームの値でデータベース検索に変更
-$sql = "SELECT * FROM `user_info`;";
-foreach ($mysqli_query($dbh,$sql) as $row){
-	if($row["mail_address"] !== $login_data[0]){
-		$_SESSION["login_check"]="failed";
-	}else if($row["pass"] !== md5($login_data[1])){ 
-		$_SESSION["login_check"]="failed";
-	}else{
-		$_SESSION["login_check"] ="success";
-	}
+//ログインフォームの値がデータベースの内容と一致するか調べる
+$login_data['mail_address']=$_POST["login_address"];
+$login_data['pass'] = md5($_POST["login_pass"]);
+//フォームの値でデータベース検索	
+$sql = "SELECT * FROM `user_info` where mail_address='".$login_data['mail_address']."' AND pass='".$login_data['pass']."';";
+
+$stmt = $dbh->query($sql);
+
+if(empty($stmt->fetch_assoc())){
+		$_SESSION["login_check"] ="failed";
+}else{
+	$_SESSION["login_check"] ="success";
 }
 
 ?>
@@ -23,13 +22,11 @@ foreach ($mysqli_query($dbh,$sql) as $row){
 <head>
 	<meta charset="UTF-8">
 	<?php
-	if($_SESSION["login_check"]=="success"){
-		echo '<meta http-equiv="refresh" content="0;URL=../php_homework03/top.php">';
-	}else if($_SESSION["login_check"]=="failed"){
-		echo '<meta http-equiv="refresh" content="0;URL=../php_homework03/index.php">';
-	}
-	?>
-	<title>ログイン後のページ</title>
+	if($_SESSION["login_check"]=="success") {?>
+	<meta http-equiv="refresh" content="0;URL=../php_homework03/index.php">
+	<?php }else if($_SESSION["login_check"]=="failed"){ ?>
+	<meta http-equiv="refresh" content="0;URL=../php_homework03/top.php">
+	<?php } ?>
 </head>
 <body>
 </body>
